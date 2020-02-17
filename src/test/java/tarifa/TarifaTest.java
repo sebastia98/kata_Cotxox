915cb3504fee062f224cbdf8efc296f41db27149
sebastia98 <sadrover@cifpfbmoll.eu>
2020-02-17 23:57:39 +0100

    Test: Test de costes

---
+++ src/test/java/tarifa/TarifaTest.java
@@ -0,0 +1,15 @@
+package tarifa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TarifaTest {
+
+	@Test
+	public void costeDistanciatest() {
+		assertEquals(Tarifa.getCosteDistancia(10.0), 13.5, 0.0);
+		assertEquals(Tarifa.getCosteMinuto(10), 3.5, 0.0);
+	}
+	/*CosteTotal comprobado con el test de carrera de costeTotalEsperado*/
+}
