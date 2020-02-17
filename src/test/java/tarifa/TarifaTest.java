ed745f7d0cb94e690dc0db6e9094b84f216bc9fd
sebastia98 <sadrover@cifpfbmoll.eu>
2020-02-18 00:09:45 +0100

    Test: Test atributos Tarifa

--- src/test/java/tarifa/TarifaTest.java
+++ src/test/java/tarifa/TarifaTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TarifaTest {
 
 	@Test
-	public void costeDistanciatest() {
+	public void costes() {
 		assertEquals(Tarifa.getCosteDistancia(10.0), 13.5, 0.0);
 		assertEquals(Tarifa.getCosteMinuto(10), 3.5, 0.0);
 	}
