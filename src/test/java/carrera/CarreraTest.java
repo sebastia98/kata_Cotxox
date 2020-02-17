b1963cf7c839d7fa64f7911d11ed8b67e7dfd6ec
sebastia98 <sadrover@cifpfbmoll.eu>
2020-02-17 19:08:49 +0100

    test: CrearCarreraTest y comprobarCosteEsperado.

--- src/test/java/carrera/CarreraTest.java
+++ src/test/java/carrera/CarreraTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import tarifa.Tarifa;
+
 public class CarreraTest {
 
 	@Test
@@ -23,7 +25,21 @@ public class CarreraTest {
 		assertEquals(carrera.getDistancia(), distancia, 0);
 		assertEquals(carrera.getTiempoEstimado(), tiempoEsperadoMinutos, 0);
 		assertEquals(carrera.getTarjetaCredito(), tarjetaCredito);
-		
+	}
+	
+	@Test
+	public void comprobarCosteEsperado() {
+		String tarjetaCredito = "4916119711304546";
+		String origen = "Aeroport Son Sant Joan";
+		String destino = "Magaluf";
+		double distancia = 7.75;
+		int tiempoEsperadoMinutos = 10;
+		Carrera carrera = new Carrera(tarjetaCredito);
+		carrera.setOrigen(origen);
+		carrera.setDestino(destino);
+		carrera.setDistancia(distancia);
+		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+		assertEquals(Tarifa.getCosteTotal(carrera), carrera.getCosteEsperado());
 	}
 
 }
